d67f7ca936da4eb568ba44a48c14fc4dcc7c9fa1
fatmakhv <fatmaba@gmail.com>
2023-10-08 22:04:45 +0100

    bug fix

--- frontend/src/app/components/EventsList/EventsList.tsx
+++ frontend/src/app/components/EventsList/EventsList.tsx
@@ -5,13 +5,17 @@ import { EventType } from "../../../shared/types";
 import classes from "./EventsList.module.css";
 
 const EventsList = ({events}: { events: EventType[]}) : JSX.Element => {
+	if (!Array.isArray(events)) {
+		return <div>No events to display</div>;
+	}
+
 	return (
 		<div className={classes.events}>
 			<h1>All Events</h1>
 			<ul className={classes.list}>
 				{events.map((event: EventType) => (
 					<li key={event.id} className={classes.item}>
-						<Link to={event.id}>
+						<Link to={`/events/${event.id}`}>
 							<img src={event.image} alt={event.title} />
 							<div className={classes.content}>
 								<h2>{event.title}</h2>
