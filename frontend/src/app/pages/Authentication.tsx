53ded3dea5eca8a8304acf6ad362c7002d26beb4
fatmakhv <fatmaba@gmail.com>
2023-10-09 17:00:20 +0100

    Authentication's first commit

---
+++ frontend/src/app/pages/Authentication.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import AuthForm from "../components/AuthForm/AuthForm";
+
+const AuthenticationPage = (): JSX.Element => {
+	return <AuthForm />;
+};
+export default AuthenticationPage;
